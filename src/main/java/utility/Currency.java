d9417de84a0117fd62ef94e4eacf4307d93ada83
lingyu kong <lingyu_kong@cmu.ac.th>
2017-03-13 18:09:31 +0700

    Currency

---
+++ src/main/java/utility/Currency.java
@@ -0,0 +1,11 @@
+package utility;
+
+/**
+ * Created by CAMT on 13/3/2560.
+ */
+public class Currency {
+    public double exchange(String sourceCurrency, double anmount,String targetCurrency){
+        return 0.0;
+    }
+    
+}
