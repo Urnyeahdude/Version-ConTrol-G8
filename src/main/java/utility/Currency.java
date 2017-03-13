18892d8e23e2e2c6820d7d6e4abdae6889bc3e37
DESKTOP-9CKSS1Q\CAMT <kevin_cox@cmu.ac.th>
2017-03-13 18:35:38 +0700

    Update maven pom.xml

--- src/main/java/utility/Currency.java
+++ src/main/java/utility/Currency.java
@@ -4,7 +4,9 @@ package utility;
  * Created by CAMT on 13/3/2560.
  */
 public class Currency {
-    public double exchange(String sourceCurrency, double anmount,String targetCurrency){
+    public double exchange(String sourceCurrency, double anmount,String targetCurrency)
+    {
+
         return 0.0;
     }
     
